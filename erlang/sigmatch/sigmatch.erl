4fea29f0597a4e2c55658008be0859f57a53b9c0
Richard Ramsden <richard@rramsden.ca>
2012-07-24 15:47:08 -0700

    handle regular expressions for patterns

--- erlang/sigmatch/sigmatch.erl
+++ erlang/sigmatch/sigmatch.erl
@@ -8,7 +8,7 @@
 %%%
 %%%             +--------------+               +---------------+
 %%%      Text   |              |     maybe     |               |
-%%%  +--------->|   SigMatch   |+------------->|    ETS Dict   |
+%%%  +--------->|   SigMatch   |+------------->|     Dict      |
 %%%             |              |+-------+      |               |
 %%%             +--------------+        |      +---------------+
 %%%                    +                |          |       |    
@@ -16,8 +16,8 @@
 %%%                    |                |          |       |
 %%%                    v                v          v       v
 %%%
-%%% Sigmatch returns either maybe, match, or nomatch depending on
-%%% the input.
+%%% In this implementation we use a Dict which stores a list of possible
+%%% matches when a `maybe` occurs.
 %%% @end
 %%%-------------------------------------------------------------------
 -module(sigmatch).
@@ -25,19 +25,22 @@
 
 -define(l2b(X), list_to_binary(X)).
 -define(b2l(X), binary_to_list(X)).
+-define(t2b(X), term_to_binary(X)).
+-define(b2t(X), binary_to_term(X)).
 
-% leaf nodes sit at the bottom of the truncated
-% trie described in the SigMatch whitepaper. They
-% will either contain a bloomfilter or a list of possible
-% matches or both.
--record(leaf_node, {
-    bloom,
+-record(signature, {
+    pattern, % re
+    chomped 
+}).
+
+-record(leaf, {
+    bloom, 
     matches = []
 }).
 
 -record(tables, {
-    trie,
-    dict
+    trie = dict:new() :: dict:dict(any(), #leaf{}),
+    dict = dict:new() :: dict:dict(any(), [string()])
 }).
 
 % tuning parameters for sigtree
@@ -47,7 +50,6 @@
 % misc
 -define(ALPHABET, 256).
 -define(BGRAM_TABLE_SIZE, (?ALPHABET * ?ALPHABET)).
--define(default(X, Fun), case X of undefined -> Fun(); X -> X end).
 
 %%%
 %% @doc
@@ -55,21 +57,34 @@
 %%
 %% Generates a sigtree for a list of patterns
 %% @end
-new(Signatures) ->
-    Trie = dict:new(),
-    Dict = dict:new(),
-    Tables0 = #tables{trie=Trie, dict=Dict},
-    Cover = lists:reverse(cover(Signatures)),
-    Tables1 = build_index(Tables0, Signatures, Cover),
+new(Patterns) ->
+    Signatures = lists:map(fun(P) -> 
+        #signature{pattern=P, chomped=regex_chomp(P)}
+    end, Patterns),
+    Cover = cover(Signatures),
+    Tables0 = build_index(#tables{}, Signatures, Cover),
+    Tables1 = compress_tree(Tables0),
     {sigtree, Tables1}.
+
+compress_tree(#tables{dict=Dict0, trie=Trie0} = Tables) ->
+    Dict1 = dict:map(fun(_, Patterns) -> compress(Patterns) end, Dict0),
+    Trie1 = dict:map(
+            % bloomfilters are fat, compress them down
+            fun(_, #leaf{bloom=B0} = Leaf) -> 
+                    B1 = compress(B0),
+                    Leaf#leaf{bloom=B1}
+            end, Trie0
+    ),
+    Tables#tables{dict=Dict1, trie=Trie1}.
 
 %%%
 %% @doc
 %% match/2
 %%
 %% Since were trying to match patterns against a text string
-%% we need to walk the text character by characters until we hit
-%% a match in our ETS ordered set.
+%% we need to walk the text character by character until we hit
+%% a match. Once we hit a match we check the leafs bloomfilter to
+%% see if its in the set. If it is use a lookup dict for possible matches
 %% @end
 match(Text, {sigtree, Tables}) ->
     walk_text(Tables, Text, Text).
@@ -78,110 +93,100 @@ walk_text(_, _, Substr) when length(Substr) =< ?B -> nomatch;
 walk_text(#tables{trie=Trie, dict=Dict} = Tables, Text, [_|T] = Substr) ->
     [B, Beta] = split(Substr, ?B, ?BETA),
     TrieIndex = B,
-    DictIndex = string:substr(Substr, 1, ?B + ?BETA),
 
     case dict:find(TrieIndex, Trie) of
-        {ok, Leaf} ->
-            case check_leaf(Dict, DictIndex, Leaf, Text, Beta) of
+        {ok, #leaf{matches=Matches, bloom=C}} ->
+            Bloom = uncompress(C),
+            InMatches = re_match_any(Text, Matches),
+            DictIndex = string:substr(Substr, 1, ?B + ?BETA),
+            case (InMatches orelse (in_bloom(Beta, Bloom) andalso in_dict(Dict, DictIndex, Text))) of
                 true -> match;
                 false -> walk_text(Tables, Text, T)
             end;
-        error ->
+        _ ->
             walk_text(Tables, Text, T)
     end.
 
-check_leaf(Dict, DictIndex, Leaf, Text, Beta) ->
-    Matches = Leaf#leaf_node.matches,
-    BF = Leaf#leaf_node.bloom,
-    InMatches = lists:any(fun ([]) -> true; % the index matched the string
-                              (SubStr) -> is_prefix(SubStr, Beta)
-                          end, Matches),
-    InMatches orelse (in_bloom(Beta, BF) andalso in_dict(Dict, DictIndex, Text)).
-
-in_bloom(_, undefined) -> false;
-in_bloom(E, BF) -> bloom:is_element(E, BF).
+in_bloom(_, undefined) ->
+    false;
+in_bloom(Value, BF) ->
+    bloom:is_element(Value, BF).
 
 in_dict(Dict, DictIndex, Text) ->
     case dict:find(DictIndex, Dict) of
-        error ->
-            false;
-        {ok, Matches} ->
-            lists:any(fun(Pattern) -> re:run(Text, Pattern) =/= nomatch end, Matches)
+        {ok, C} ->
+            Matches = uncompress(C),
+            re_match_any(Text, Matches);
+        _ ->
+            false
     end.
 
-is_prefix(SubStr, RS) ->
-    string:str(RS, SubStr) == 1.
+re_match_any(Text, Matches) ->
+    lists:any(fun(Pattern) -> re:run(Text, Pattern) =/= nomatch end, Matches).
+
+compress(Leaf) ->
+    zlib:compress(?t2b(Leaf)).
+uncompress(Leaf) ->
+    ?b2t(zlib:uncompress(Leaf)).
 
 build_index(Tables, [], _) -> Tables;
 build_index(Tables0, [Sig|T], Cover) ->
     Tables1 = case walk_bgrams(Tables0, Sig, Cover) of
         {match, Substring} -> add_index(Tables0, Sig, ?b2l(Substring));
-        nomatch -> add_index(Tables0, Sig, Sig)
+        nomatch -> add_index(Tables0, Sig, Sig#signature.chomped)
     end,
     build_index(Tables1, T, Cover). 
 
 walk_bgrams(_, _, []) -> nomatch;
-walk_bgrams(Tables, Sig, [BGram | T]) ->
-    case representative_substring(?l2b(Sig), ?l2b(BGram)) of
+walk_bgrams(Tables, #signature{chomped=Chomped} = Sig, [BGram | T]) ->
+    case representative_substring(?l2b(Chomped), ?l2b(BGram)) of
         {match, Substring} ->
             {match, Substring};
         nomatch ->
             walk_bgrams(Tables, Sig, T)
     end.
 
-%%%
-%% @private
-%% @doc
-%% We construct the Trie mentioned in the SigMatch whitepaper
-%% by using an ordered ETS Table. The first ?B characters are 
-%% stored as an index into the ETS table while the following ?BETA
-%% characters are hashed into a bloom filter or appended to a linked list.
-%% If the signature is shorter than ?B + ?BETA characters we store possible
-%% matches in a linked list.
-%% @end
 add_index(#tables{trie=Trie0, dict=Dict0}, Signature, Substring) ->
-    Trie1 = add_trie_index(Trie0, Substring),
+    Trie1 = add_trie_index(Trie0, Signature, Substring),
     Dict1 = add_dict_index(Dict0, Signature, Substring),
     #tables{trie=Trie1, dict=Dict1}.
 
+%%% store all patterns for future lookup
+add_dict_index(Dict, _, Substring) when length(Substring) < (?B + ?BETA) ->
+    Dict;
 add_dict_index(Dict, Sig, Substring) ->
     Index = string:substr(Substring, 1, ?B + ?BETA),
 
     case dict:find(Index, Dict) of
         error ->
-            dict:store(Index, [Sig], Dict);
+            dict:store(Index, [Sig#signature.pattern], Dict);
         {ok, Matches} ->
-            dict:store(Index, [Sig|Matches], Dict)
+            dict:store(Index, [Sig#signature.pattern|Matches], Dict)
     end.
 
-add_trie_index(Trie, Substring) ->
+%%% store bloomfilters for runtime checks
+add_trie_index(Trie, Signature, Substring) ->
     [TrieIndex, Rest] = split(Substring, ?B, ?BETA),
+    add_trie_index(Trie, TrieIndex, Signature, Rest).
 
+add_trie_index(Trie, TrieIndex, Sig, Rest) when length(Rest) < ?BETA ->
     case dict:find(TrieIndex, Trie) of
         error ->
-            dict_insert(Trie, #leaf_node{}, TrieIndex, Rest);
-        {ok, Leaf} ->
-            dict_insert(Trie, Leaf, TrieIndex, Rest)
-    end.
-
-split(Str, _, _) when length(Str) =< ?B ->
-    [Str, []];
-split(Str, Start, End) ->
-    P1 = string:substr(Str, 1, Start),
-    P2 = string:substr(Str, Start + 1, End),
-    [P1, P2].
-
-dict_insert(Trie, Leaf0, Index, SubStr) when length(SubStr) < ?BETA ->
-    Matches0 = Leaf0#leaf_node.matches,
-    Matches1 = [SubStr|Matches0],
-    Leaf1 = Leaf0#leaf_node{matches=Matches1},
-    dict:store(Index, Leaf1, Trie);
-dict_insert(Trie, Leaf0, Index, SubStr) ->
-    Bloomfilter = Leaf0#leaf_node.bloom,
-    B1 = ?default(Bloomfilter, fun() -> bloom:sbf(26700) end),
-    B2 = bloom:add_element(SubStr, B1),
-    Leaf1 = Leaf0#leaf_node{bloom=B2},
-    dict:store(Index, Leaf1, Trie).
+            dict:store(TrieIndex, #leaf{matches=[Sig#signature.pattern]}, Trie);
+        {ok, #leaf{matches=Matches} = Leaf0} ->
+            Leaf1 = Leaf0#leaf{matches=[Sig#signature.pattern | Matches]},
+            dict:store(TrieIndex, Leaf1, Trie)
+    end;
+add_trie_index(Trie, TrieIndex, _, Rest) ->
+    BFNew = case dict:find(TrieIndex, Trie) of
+        error ->
+            BF = bloom:sbf(26700),
+            #leaf{bloom = bloom:add_element(Rest, BF)};
+        {ok, #leaf{bloom=BF0} = Leaf} ->
+            BF1 = case BF0 of undefined -> bloom:sbf(26700); _ -> BF0 end,
+            Leaf#leaf{bloom = bloom:add_element(Rest, BF1)}
+    end,
+    dict:store(TrieIndex, BFNew, Trie).
 
 %%%
 %% @private
@@ -189,7 +194,7 @@ dict_insert(Trie, Leaf0, Index, SubStr) ->
 %% Constructs an effecient B-Gram cover given a signature set
 %% @end
 cover(Signatures) ->
-    lists:reverse(cover(Signatures, [])).
+    cover(Signatures, []).
 cover(Signatures0, Acc) ->
     TabId = ets:new(frequency_table, [set]),
     build_cover(TabId, Signatures0),
@@ -212,7 +217,7 @@ prune(SigSet, BGram) ->
 prune([], Pruned, _) ->
     Pruned;
 prune([Sig|T], Pruned, BGram) ->
-    case representative_substring(?l2b(Sig), ?l2b(BGram)) of
+    case representative_substring(?l2b(Sig#signature.chomped), ?l2b(BGram)) of
         {match, _} ->
             % discard the signature if it is a representative
             % substring. These are (b + Beta) bytes long
@@ -271,7 +276,7 @@ build_cover(TabId, Signatures) ->
 build_cover(_Pos, _TabId, []) ->
     ok;
 build_cover(Pos, TabId, [Sig | Rest]) ->
-    extract_covers_from_string(Sig, TabId),
+    extract_covers_from_string(Sig#signature.chomped, TabId),
     build_cover(Pos + 1, TabId, Rest).
 
 extract_covers_from_string(Str, _TabId) when length(Str) < (?B + ?BETA) ->
@@ -284,19 +289,25 @@ extract_covers_from_string([_|T] = Str, TabId) ->
     end,
     extract_covers_from_string(T, TabId).
 
+regex_chomp(Pattern0) ->
+    Pattern1 = re:replace(Pattern0, "[\\^\\$\\|\\?\\*\\+\\(\\)]+", "", [global, {return, list}]),
+    Pattern2 = re:replace(Pattern1, "\\\\[a-zA-Z]", "", [global, {return, list}]),
+    re:replace(Pattern2, "\\\\.", ".", [global, {return, list}]).
+
+split(Str, _, _) when length(Str) =< ?B ->
+    [Str, []];
+split(Str, Start, End) ->
+    P1 = string:substr(Str, 1, Start),
+    P2 = string:substr(Str, Start + 1, End),
+    [P1, P2].
+
+
 -ifndef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 
-%% small patterns which are less than the size
-%% of the representative substring are handled using
-%% linked list lookups instead of bloom filters
 small_pattern_test() ->
-    % NOTE: we can't have patterns less than a ?B
-    % characters since we don't use a trie. We index
-    % possible matches by taking ?B characters from a string
-    % and storing it as an index into an ETS table
     Patterns = [
-        "cat",
+        "\\bcat\\b",
         "cow",
         "dog",
         "i ",
@@ -304,6 +315,7 @@ small_pattern_test() ->
     ],
     S = sigmatch:new(Patterns),
     ?assertEqual(match, sigmatch:match("there is a cat in the bag", S)),
+    ?assertEqual(nomatch, sigmatch:match("catz", S)),
     ?assertEqual(match, sigmatch:match("cow", S)),
     ?assertEqual(match, sigmatch:match(" dog", S)),
     ?assertEqual(match, sigmatch:match("dog ", S)),
@@ -314,20 +326,29 @@ small_pattern_test() ->
     ?assertEqual(nomatch, sigmatch:match("foobar", S)),
     ?assertEqual(nomatch, sigmatch:match("the lazy foo jumped over the moon", S)).
 
-%% Bigger patterns will use bloom filters to check
-%% for existence.
+regex_chomp_test() ->
+    Patterns = [
+        "^exact match$",
+        "\\bboundary match\\b",
+        "^255\\.255\\.255\\.*$"
+    ],
+    Expected = [
+        "exact match",
+        "boundary match",
+        "255.255.255."
+    ],
+    ?assertEqual(lists:map(fun(P) -> regex_chomp(P) end, Patterns), Expected).
+
 big_pattern_test() ->
     Patterns = [
         "will this big pattern match :D",
         "will this big pattern match ;D",
-        "will this big pattern match :O"
+        "will this big pattern match :O",
+        "this clearly should pass the test"
     ],
     S = sigmatch:new(Patterns),
-
-    % This will match because we look at ?B characters as the index
-    % into our ETS then the following ?BETA characters are hashed into
-    % a bloom filter checking for existence.
     ?assertEqual(match, sigmatch:match("will this big pattern match ;D", S)),
-    ?assertEqual(nomatch, sigmatch:match("will this big pattern match ;X", S)).
+    ?assertEqual(nomatch, sigmatch:match("will this big pattern match ;X", S)),
+    ?assertEqual(match, sigmatch:match("I think this clearly should pass the test", S)).
 
 -endif.
